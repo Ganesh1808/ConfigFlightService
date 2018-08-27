54761fa76f9a597f96d4a7cbf0a9f10bd98ef8bf
Ganesh1808 <ganeshrrrr18@gmail.com>
2018-08-27 11:57:26 +0530

    .

---
+++ src/main/java/com/project/ConfigFlightService/ConfigFlightServiceApplication.java
@@ -0,0 +1,12 @@
+package com.project.ConfigFlightService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigFlightServiceApplication.class, args);
+	}
+}
